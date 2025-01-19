412d40b9714e74f4b1d6a22a28b6ba894f966ac7
eason034056 <129682160+eason034056@users.noreply.github.com>
2025-01-19 14:49:45 +0800

    modify header

--- components/header.tsx
+++ components/header.tsx
@@ -34,35 +34,35 @@ export default function Header() {
         <nav className={`${isMenuOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`}>
           <ul className="flex flex-col md:flex-row px-4 md:px-0 md:space-x-4 font-bold space-y-4 md:space-y-0 mt-4 md:mt-0">
             
-            <li><Link href="/tutor-registration" className="hover:underline block text-right md:text-left text-[#0F1035]">教師登錄</Link></li>
-            <li><Link href="/tutors" className="hover:underline block text-right md:text-left text-[#0F1035]">家教老師</Link></li>
-            <li><Link href="/tutor-cases" className="hover:underline block text-right md:text-left text-[#0F1035]">家教案件</Link></li>
+            <li><Link href="/tutor-registration" onClick={() => setIsMenuOpen(false)} className="hover:underline block text-right md:text-left text-[#0F1035]">教師登錄</Link></li>
+            <li><Link href="/tutors" onClick={() => setIsMenuOpen(false)} className="hover:underline block text-right md:text-left text-[#0F1035]">家教老師</Link></li>
+            <li><Link href="/tutor-cases" onClick={() => setIsMenuOpen(false)} className="hover:underline block text-right md:text-left text-[#0F1035]">家教案件</Link></li>
             <li className="group relative">
               <span className="hover:underline cursor-pointer block text-right md:text-left text-[#0F1035]">教師須知</span>
-              <div className="absolute right-0 top-full hidden group-hover:block bg-[#415F9D] text-white rounded-md shadow-lg p-2 z-50 min-w-[120px] whitespace-nowrap">
-                <Link href="/process" className="block px-4 py-2 hover:bg-white hover:text-[#0F1035] rounded-sm text-right md:text-left text-white">
+              <div className="md:bg-[#415F9D] md:absolute md:right-0 md:top-full hidden group-hover:block text-black md:text-white rounded-md md:shadow-lg md:p-2 md:z-50 md:min-w-[140px] md:whitespace-nowrap">
+                <Link href="/process" onClick={() => setIsMenuOpen(false)} className="block px-4 py-2 hover:bg-white hover:text-[#0F1035] rounded-sm text-right md:text-left">
                   接案流程
                 </Link>
               </div>
             </li>
             <li className="group relative">
               <span className="hover:underline cursor-pointer block text-right md:text-left text-[#0F1035]">家長須知</span>
-              <div className="bg-[#415F9D] absolute right-0 top-full hidden group-hover:block text-white rounded-md shadow-lg p-2 z-50 min-w-[140px] whitespace-nowrap">
-                <Link href="/notice" className="block px-4 py-2 hover:bg-white hover:text-[#0F1035] rounded-sm text-right md:text-left">
+              <div className="md:bg-[#415F9D] md:absolute md:right-0 md:top-full hidden group-hover:block text-black md:text-white rounded-md md:shadow-lg md:p-2 md:z-50 md:min-w-[140px] md:whitespace-nowrap">
+                <Link href="/notice" onClick={() => setIsMenuOpen(false)} className="block px-4 py-2 hover:bg-white hover:text-[#0F1035] rounded-sm text-right md:text-left">
                   注意事項
                 </Link>
-                <Link href="/comparison" className="block px-4 py-2 hover:bg-white hover:text-[#0F1035] rounded-sm text-right md:text-left">
+                <Link href="/comparison" onClick={() => setIsMenuOpen(false)} className="block px-4 py-2 hover:bg-white hover:text-[#0F1035] rounded-sm text-right md:text-left">
                   與補習班之比較
                 </Link>
                 {/* <Link href="/license" className="block px-4 py-2 hover:bg-white hover:text-[#0F1035] rounded-sm text-right md:text-left">
                   政府立案
                 </Link> */}
-                <Link href="/pricing" className="block px-4 py-2 hover:bg-white hover:text-[#0F1035] rounded-sm text-right md:text-left">
+                <Link href="/pricing" onClick={() => setIsMenuOpen(false)} className="block px-4 py-2 hover:bg-white hover:text-[#0F1035] rounded-sm text-right md:text-left">
                   家教費用參考
                 </Link>
               </div>
             </li>
-            <li><Link href="/case-upload" className="hover:underline block text-right md:text-left bg-[#00A1B4] px-4 text-white rounded-3xl">找家教</Link></li>
+            <li className="text-right"><Link href="/case-upload" onClick={() => setIsMenuOpen(false)} className="hover:underline inline-block text-right md:text-left bg-[#00A1B4] px-4 text-white rounded-3xl">找家教</Link></li>
           </ul>
         </nav>
       </div>

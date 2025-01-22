f101722db3ae2024bfec4b70defda6ead85ee271
eason034056 <129682160+eason034056@users.noreply.github.com>
2025-01-22 15:16:52 +0800

    add redirect

--- next.config.js
+++ next.config.js
@@ -2,7 +2,33 @@
 const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com']
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.tutor-matching.tw',
+          },
+        ],
+        destination: 'https://tutor-matching.tw/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'tutor-matching.vercel.app',
+          },
+        ],
+        destination: 'https://tutor-matching.tw/:path*',
+        permanent: true,
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig 

c91d5e1b13d6aef1bd0138afe0469b3e8a626908
eason034056 <129682160+eason034056@users.noreply.github.com>
2025-01-22 15:24:29 +0800

    remove redirect method

--- next.config.js
+++ next.config.js
@@ -2,45 +2,6 @@
 const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com']
-  },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.tutor-matching.tw',
-          },
-        ],
-        destination: 'https://tutor-matching.tw/:path*',
-        permanent: true,
-      },
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'tutor-matching.vercel.app',
-          },
-        ],
-        destination: 'https://tutor-matching.tw/:path*',
-        permanent: true,
-      }
-    ]
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Host',
-            value: 'tutor-matching.tw'
-          }
-        ],
-      },
-    ]
   }
 }
 

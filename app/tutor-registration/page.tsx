87b6590b8b1840f5ce34141e76bfc0ff3b27e3de
eason034056 <129682160+eason034056@users.noreply.github.com>
2025-07-13 16:34:04 +0800

    update line id

--- app/tutor-registration/page.tsx
+++ app/tutor-registration/page.tsx
@@ -16,7 +16,7 @@ export default function TutorRegistrationPage() {
           <CardDescription>請填寫以下表單以登錄成為教師<br />
           登錄完後會進行身份審核，審核時間大約1~2天，審核完畢後會收到email傳送教師編號<br />
           請記得您的教師編號，後續接案都需使用教師編號進行應徵<br />
-          或是直接私訊我們，LINE ID：@home-tutor-tw</CardDescription>
+          或是直接LINE私訊我們，LINE ID：home-tutor-tw</CardDescription>
         </CardHeader>
         <CardContent>
           <TutorRegistrationForm />
